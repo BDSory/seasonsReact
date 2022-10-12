f553dc1c58ce1930e151efcfa46af87b017b87ef
Bryan Sory <superbee45@gmail.com>
2022-10-12 16:58:07 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
+//https://stackoverflow.com/questions/54086244/semantic-ui-react-style-not-loaded
+//if semantic still not loading, do link above
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import  ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  state = {lat : null, errorMessage : ''};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+    );
+  }
+
+//below is 'conditional rendering'; simplest version
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat = {this.state.lat} />
+    }
+    return <Spinner />;
+  }
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container); 
+root.render(<App />);
